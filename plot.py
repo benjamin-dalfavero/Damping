c7dd41e18e51d37010ca19b3e0002dd136fffabd
Benjamin DalFavero <bdalfavero@scu.edu>
2018-12-19 14:16:56 -0800

    updated script with new methods

--- plot.py
+++ plot.py
@@ -1,25 +1,44 @@
-'''
-plot reduction of acceleration vs damping coefficient
-for a given component mass
-'''
-
 import damper as d
 import matplotlib.pyplot as plt
 import numpy as np
 
-def plotCVals(shock, sys, cs):
-    '''
-    plot reduction efficiency for various values of C
-    when subjected to a shockwave, and with a component of
-    a certain mass.
-    shock: (acceleration, ptp time)
-    sys: Damper.System
-    cs: range of values for c
-    '''
-    a, dt = shock
-    # basically a map of reduction over values of c
+def mapCs(sys, cs):
+    # map reduction over values of c
+    old_c = sys.damper.coef
     accum = []
     for c in cs:
         sys.damper.coef = c
-        accum += [sys.reduction(a, dt)]
-    plt.plot(cs, accum)
+        accum.append(sys.force_ratio())
+    sys.damper.coef = old_c
+    return accum
+
+def plotCase(name, sys, c_vals, a_vals):
+    # plot redution vs c for all values of a
+    for a in a_vals:
+        sys.wave.accel = a
+        force_vals = mapCs(sys, c_vals)
+        plt.plot(c_vals, force_vals)
+    # put titles on graph
+    plt.title(name)
+    plt.xlabel('C')
+    plt.ylabel('Force Ratio')
+    filename = name.replace(' ', '_') + ".jpg"
+    plt.savefig(filename)
+    plt.clf()
+
+# define test case.
+g = 9.81
+a_vals = [0.5 * g, g, 1.5 * g]
+shockwave = d.Shockwave(4.905, 0.2)
+
+ld500 = d.Damper(6.55, 1.0, 4.0)
+ld500_sys = d.System(907.18, ld500, shockwave)
+ld500_c_vals = np.arange(1.0, 10.0, 0.01)
+
+ld700 = d.Damper(16.5, 1.0, 10)
+ld700_sys = d.System(1361, ld700, shockwave)
+ld700_c_vals = np.arange(3.0, 20.0, 0.01)
+
+# plot
+plotCase("LD500 2000lbs", ld500_sys, ld500_c_vals, a_vals)
+plotCase("LD700 3000lbs", ld700_sys, ld700_c_vals, a_vals)
